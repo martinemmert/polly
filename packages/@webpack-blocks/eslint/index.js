36cef22c534f36622c030c1ce928673398d0b79c
Martin Emmert <martin@martinemmert.de>
2017-01-10 23:26:49 +0100

    added @webpack-blocks/eslint code

---
+++ packages/@webpack-blocks/eslint/index.js
@@ -0,0 +1,42 @@
+/**
+ * ESLint Webpack Block
+ *
+ * @see https://github.com/MoOx/eslint-loader
+ */
+
+const {webpack} = require("@webpack-blocks/webpack2");
+
+module.exports = eslint;
+
+/**
+ * @return {Function}
+ */
+function eslint() {
+
+    let plugin = new webpack.LoaderOptionsPlugin({
+        options: {
+            eslint: {
+                configFile: "./.eslintrc",
+                cache: true,
+                failOnError: false,
+                failOnWarning: false,
+            },
+        },
+    });
+
+    return (context, webpackConfig) => {
+        return {
+            module: {
+                loaders: [
+                    {
+                        enforce: "pre",
+                        test: /\.jsx?$/,
+                        loader: "eslint-loader",
+                        exclude: /node_modules/,
+                    },
+                ],
+            },
+            plugins: [plugin],
+        };
+    };
+}
